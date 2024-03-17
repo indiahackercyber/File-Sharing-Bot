05067856f6c3a19c0302ce6b0d576cecc08d5962
INDIAHACKER_ICS <140348179+indiahackercyber@users.noreply.github.com>
2024-03-17 23:02:47 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,4 @@
-#(©)Codexbotz
+#(Â©)Codexbotz
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>â—‹ Creator : <a href='tg://user?id={OWNER_ID}'>@Sksksk99</a>\nâ—‹ Language : <code>Python3</code>\nâ—‹ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\nâ—‹ Source Code : <a href='@SKsksk99'>Click here</a>\nâ—‹ Channel : @INDIAHACKER_ICS\nâ—‹ Hacking and course : @INDIAHACKER_ICS</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("ðŸ”’ Close", callback_data = "close")
                     ]
                 ]
             )
